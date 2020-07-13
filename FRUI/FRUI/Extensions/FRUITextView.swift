bb2182352620378bf5165582e893501ef9f90392
James Go <jhoongo@gmail.com>
2020-07-13 12:12:39 -0700

    [iOS] SDK access to the Page Node's header and description property

---
+++ FRUI/FRUI/Extensions/FRUITextView.swift
@@ -0,0 +1,29 @@
+// 
+//  FRUITextView.swift
+//  FRUI
+//
+//  Copyright (c) 2020 ForgeRock. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
+//
+
+
+import Foundation
+
+extension UITextView {
+    func setHTMLString(_ value: String) {
+        if let stringData = value.data(using: .utf8) {
+            do {
+                let attributedString = try NSAttributedString(data: stringData, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil)
+                self.attributedText = attributedString
+            }
+            catch {
+                self.text = value
+            }
+        }
+        else {
+            self.text = value
+        }
+    }
+}
