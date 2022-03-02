a50f393bc6ea88b664933e0855d7d350e5441d0a
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2022-03-01 22:53:36 -0500

    Roll back some initial changes

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let frSDKVersion = "3.2.0"
-
 let package = Package (
     name: "ForgeRock-iOS-SDK",
     platforms: [
@@ -24,11 +22,7 @@ let package = Package (
     targets: [
         .target(name: "cFRAuth", dependencies: [], path: "FRAuth/FRAuth/SharedC/Sources"),
         .target(name: "cFRAuthenticator", dependencies: [], path: "FRAuthenticator/FRAuthenticator/SharedC/Sources"),
-        .target(name: "FRCore", path: "FRCore/FRCore", exclude: ["Info.plist", "FRCore.h"],
-                cSettings: [
-            .headerSearchPath("../../"),
-            .define("FR_SDK_VERSION", to: frSDKVersion),
-          ]),
+        .target(name: "FRCore", path: "FRCore/FRCore", exclude: ["Info.plist", "FRCore.h"]),
         .target(name: "FRAuth", dependencies: [.target(name: "FRCore"), .target(name: "cFRAuth")], path: "FRAuth/FRAuth", exclude: ["Info.plist", "FRAuth.h", "SharedC/Sources/include/JBUtil.h", "SharedC/Sources/JBUtil.c", "SharedC/FRAuth.modulemap"]),
         .target(name: "FRProximity", dependencies: [.target(name: "FRAuth")], path: "FRProximity/FRProximity", exclude: ["Info.plist", "FRProximity.h"]),
         .target(name: "FRAuthenticator", dependencies: [.target(name: "FRAuth"), .target(name: "cFRAuthenticator")], path: "FRAuthenticator/FRAuthenticator", exclude: ["Info.plist", "FRAuthenticator.h", "SharedC/Sources/include/base32.h", "SharedC/Sources/base32.c", "SharedC/FRAuthenticator.modulemap"]),
