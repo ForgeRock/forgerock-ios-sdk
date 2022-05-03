d53bb594aa4cad626603877b96486e1ebacc3d71
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2022-05-03 19:20:17 -0400

    Add new properties to PushNotification, handle init, encoding/decoding

--- FRAuthenticator/FRAuthenticator/Model/Notification/PushNotification.swift
+++ FRAuthenticator/FRAuthenticator/Model/Notification/PushNotification.swift
@@ -33,6 +33,16 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
     var pending: Bool = true
     /// Boolean indicator of whether push notification is approved or not
     var approved: Bool = false
+    /// The JSON String containing the custom attributes added to this notification */
+    public internal(set) var customPayload: String?
+    /// Message that was received with this notification */
+    public internal(set) var message: String?
+    /// The type of push notification **/
+    public internal(set) var pushType: PushType
+    /// The numbers used in the push challenge **/
+    public internal(set) var numbersChallenge: String?
+    ///The context information to this notification. */
+    public internal(set) var contextInfo: String?
     
     
     //  MARK: - Public Properties
@@ -87,6 +97,11 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
         case challenge
         case pending
         case approved
+        case customPayload
+        case message
+        case pushType
+        case numbersChallenge
+        case contextInfo
     }
     
     
@@ -122,6 +137,16 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
         } else {
             self.timeAdded = Date()
         }
+        
+        self.customPayload = payload["p"] as? String
+        self.message = payload["m"] as? String
+        if let pushTypeString = payload["k"] as? String {
+            self.pushType = PushType(rawValue: pushTypeString) ?? PushType.default
+        } else {
+            self.pushType = PushType.default
+        }
+        self.numbersChallenge = payload["n"] as? String
+        self.contextInfo = payload["x"] as? String
     }
     
     
@@ -134,7 +159,12 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
     /// - Parameter timeAdded: Date when the notification is delivered
     /// - Parameter pending: Boolean indicator of whether or not current PushNotification is still in pending
     /// - Parameter approved: Boolean indicator of whether or not current PushNotification is already approved
-    init?(messageId: String?, challenge: String?, loadBalanceKey: String?, ttl: Double, mechanismUUID: String?, timeAdded: Double, pending: Bool, approved: Bool) {
+    /// - Parameter customPayload: JSON String containing the custom attributes
+    /// - Parameter message: message from  from APNS payload
+    /// - Parameter pushType: the type of push notification
+    /// - Parameter numbersChallenge: numbers used in the push challenge
+    /// - Parameter contextInfo: contextual information, such as location
+    init?(messageId: String?, challenge: String?, loadBalanceKey: String?, ttl: Double, mechanismUUID: String?, timeAdded: Double, pending: Bool, approved: Bool, customPayload: String?, message: String?, pushType: PushType, numbersChallenge: String?, contextInfo: String?) {
         
         guard let messageId = messageId, let challenge = challenge, let mechanismUUID = mechanismUUID else {
             return nil
@@ -148,6 +178,11 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
         self.mechanismUUID = mechanismUUID
         self.pending = pending
         self.approved = approved
+        self.customPayload = customPayload
+        self.message = message
+        self.pushType = pushType
+        self.numbersChallenge = numbersChallenge
+        self.contextInfo = contextInfo
     }
     
     
@@ -165,6 +200,11 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
         coder.encode(self.timeAdded.timeIntervalSince1970, forKey: "timeAdded")
         coder.encode(self.pending, forKey: "pending")
         coder.encode(self.approved, forKey: "approved")
+        coder.encode(self.customPayload, forKey: "customPayload")
+        coder.encode(self.message, forKey: "message")
+        coder.encode(self.pushType.rawValue, forKey: "pushType")
+        coder.encode(self.numbersChallenge, forKey: "numbersChallenge")
+        coder.encode(self.contextInfo, forKey: "contextInfo")
     }
     
     
@@ -178,8 +218,16 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
         let timeAdded = coder.decodeDouble(forKey: "timeAdded") as Double
         let pending = coder.decodeBool(forKey: "pending") as Bool
         let approved = coder.decodeBool(forKey: "approved") as Bool
+        let customPayload = coder.decodeObject(of: NSString.self, forKey: "customPayload") as String?
+        let message = coder.decodeObject(of: NSString.self, forKey: "message") as String?
+        var pushType = PushType.default
+        if let pushTypeString = coder.decodeObject(of: NSString.self, forKey: "pushType") as? String {
+            pushType = PushType(rawValue: pushTypeString) ?? PushType.default
+        }
+        let numbersChallenge = coder.decodeObject(of: NSString.self, forKey: "numbersChallenge") as String?
+        let contextInfo = coder.decodeObject(of: NSString.self, forKey: "contextInfo") as String?
         
-        self.init(messageId: messageId, challenge: challenge, loadBalanceKey: loadBalanceKey, ttl: ttl, mechanismUUID: mechanismUUID, timeAdded: timeAdded, pending: pending, approved: approved)
+        self.init(messageId: messageId, challenge: challenge, loadBalanceKey: loadBalanceKey, ttl: ttl, mechanismUUID: mechanismUUID, timeAdded: timeAdded, pending: pending, approved: approved, customPayload: customPayload, message: message, pushType: pushType, numbersChallenge: numbersChallenge, contextInfo: contextInfo)
     }
     
     
@@ -197,6 +245,11 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
         try container.encode(self.pending, forKey: .pending)
         try container.encode(self.approved, forKey: .approved)
         try container.encode(self.identifier, forKey: .identifier)
+        try container.encode(self.customPayload, forKey: .customPayload)
+        try container.encode(self.message, forKey: .message)
+        try container.encode(self.pushType, forKey: .pushType)
+        try container.encode(self.numbersChallenge, forKey: .numbersChallenge)
+        try container.encode(self.contextInfo, forKey: .contextInfo)
     }
 
     
@@ -212,8 +265,13 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
         let approved = try values.decode(Bool.self, forKey: .approved)
         let milliseconds = try values.decode(Double.self, forKey: .timeAdded)
         let timeAdded = milliseconds / 1000
+        let customPayload = try values.decode(String.self, forKey: .customPayload)
+        let message = try values.decode(String.self, forKey: .message)
+        let pushType = try values.decode(PushType.self, forKey: .pushType)
+        let numbersChallenge = try values.decode(String.self, forKey: .numbersChallenge)
+        let contextInfo = try values.decode(String.self, forKey: .contextInfo)
 
-        self.init(messageId: messageId, challenge: challenge, loadBalanceKey: loadBalanceKey, ttl: ttl, mechanismUUID: mechanismUUID, timeAdded: timeAdded, pending: pending, approved: approved)!
+        self.init(messageId: messageId, challenge: challenge, loadBalanceKey: loadBalanceKey, ttl: ttl, mechanismUUID: mechanismUUID, timeAdded: timeAdded, pending: pending, approved: approved, customPayload: customPayload, message: message, pushType: pushType, numbersChallenge: numbersChallenge, contextInfo: contextInfo)!
     }
     
     
@@ -330,4 +388,10 @@ public class PushNotification: NSObject, NSSecureCoding, Codable {
             return nil
         }
     }
+}
+                  
+public enum PushType: String, Codable {
+    case `default`
+    case challenge
+    case biometric
 }
