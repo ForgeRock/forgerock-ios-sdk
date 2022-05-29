f68fbc040ab5c66efb1aa889259299a5764b64cb
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2022-05-28 23:20:33 -0400

    Add handling of PushType.challeng and PushType.biometric to Sample App. Add location view if available in paiload

--- SampleApps/FRAuthenticatorExample/FRAuthenticatorExample/Controller/NotificationRequestViewController.swift
+++ SampleApps/FRAuthenticatorExample/FRAuthenticatorExample/Controller/NotificationRequestViewController.swift
@@ -2,7 +2,7 @@
 //  NotificationRequestViewController.swift
 //  FRAuthenticatorExample
 //
-//  Copyright (c) 2020 ForgeRock. All rights reserved.
+//  Copyright (c) 2020-2022 ForgeRock. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
@@ -10,18 +10,23 @@
 
 import UIKit
 import FRAuthenticator
+import CoreLocation
 
 class NotificationRequestViewController: BaseViewController {
-
-    @IBOutlet weak var logoImageView: UIImageView?
-    @IBOutlet weak var closeButton: UIButton?
-    @IBOutlet weak var acceptButton: UIButton?
-    @IBOutlet weak var denyButton: UIButton?
-    @IBOutlet weak var issuerLabel: UILabel?
-    @IBOutlet weak var accountNameLabel: UILabel?
-    @IBOutlet weak var descriptionLabel: UILabel?
-    @IBOutlet weak var topDescriptionLabel: UILabel?
-    @IBOutlet weak var issuerLabelTopConstraint: NSLayoutConstraint?
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var acceptButton: UIButton!
+    @IBOutlet weak var denyButton: UIButton!
+    @IBOutlet weak var issuerLabel: UILabel!
+    @IBOutlet weak var accountNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var topDescriptionLabel: UILabel!
+    @IBOutlet weak var issuerLabelTopConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var locationView: UIView!
+    @IBOutlet weak var numbersChallengeStackView: UIStackView!
     
     var notification: PushNotification?
     
@@ -29,18 +34,24 @@ class NotificationRequestViewController: BaseViewController {
         super.viewDidLoad()
         self.title = "Notification"
         
+        acceptButton.setImage(UIImage(named: "ApprovedIcon")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        denyButton.setImage(UIImage(named: "DeniedIcon")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        
+        
         guard let notification = self.notification else {
             self.dismiss(animated: true)
             return
         }
         
         if !self.isModal {
-            self.closeButton?.isHidden = true
+            self.closeButton.isHidden = true
         }
         
         if !notification.isPending {
-            self.acceptButton?.isHidden = true
-            self.denyButton?.isHidden = true
+            self.acceptButton.isHidden = true
+            self.denyButton.isHidden = true
+            self.locationView.isHidden = true
+            self.numbersChallengeStackView.isHidden = true
             
             var status = ""
             
@@ -53,28 +64,74 @@ class NotificationRequestViewController: BaseViewController {
             else if notification.isExpired {
                 status = "was expired"
             }
-
+            
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
             let timestamp = dateFormatter.string(from: notification.timeAdded)
-            self.topDescriptionLabel?.text = "This PushNotification \(status) \(notification.isExpired ? "" : "\n @ " + timestamp)"
-            self.descriptionLabel?.text = "This PushNotification \(status)."
+            self.topDescriptionLabel.text = "This PushNotification \(status) \(notification.isExpired ? "" : "\n @ " + timestamp)"
+            self.descriptionLabel.text = "This PushNotification \(status)."
+        } else {
+            
+            let pushType = notification.pushType
+            switch pushType {
+            case .default:
+                acceptButton.isHidden = false
+                denyButton.isHidden = false
+                numbersChallengeStackView.isHidden = true
+                descriptionLabel.text = "Tap Accept or Deny button to allow or reject this login request."
+            case .challenge:
+                acceptButton.isHidden = true
+                denyButton.isHidden = false
+                numbersChallengeStackView.isHidden = false
+                descriptionLabel.text = "Please choose the right number to allow this login request or tap Deny button to reject."
+                if let numbers = notification.numbersChallengeArray,
+                   let buttons = numbersChallengeStackView.arrangedSubviews as? [UIButton],
+                   numbers.count == buttons.count {
+                    for (index, button) in buttons.enumerated() {
+                        button.setTitle("\(numbers[index])", for: .normal)
+                        button.addTarget(self, action: #selector(self.numberButtonTapped), for: .touchUpInside)
+                    }
+                }
+                break
+            case .biometric:
+                acceptButton.isHidden = false
+                denyButton.isHidden = false
+                numbersChallengeStackView.isHidden = true
+                acceptButton.setTitle("Accept with Biometric", for: .normal)
+                descriptionLabel.text = "Tap Accept with Biometric to allow this login request or tap Deny button to reject it."
+                
+            }
+            
+            //location
+            if let contextInfoData = notification.contextInfo?.data(using: .utf8),
+               let contextinfo = try? JSONSerialization.jsonObject(with: contextInfoData, options: []) as? [String : Any],
+               let location = contextinfo["location"] as? [String: Any],
+               let latitude = location["latitude"] as? Double,
+               let longitude = location["longitude"] as? Double{
+                getAddressFrom(latitude: latitude, longitude: longitude) { address in
+                    self.locationLabel.text = address
+                }
+                
+            } else {
+                locationView.isHidden = true
+            }
+            
         }
         
-        self.logoImageView?.image = nil
-        self.issuerLabelTopConstraint?.constant = 125
+        self.logoImageView.image = nil
+        self.issuerLabelTopConstraint.constant = 125
         if let mechanism = FRAClient.shared?.getMechanism(notification: notification) {
-            self.issuerLabel?.text = mechanism.issuer
-            self.accountNameLabel?.text = mechanism.accountName
+            self.issuerLabel.text = mechanism.issuer
+            self.accountNameLabel.text = mechanism.accountName
             
             if let account = FRAClient.shared?.getAccount(mechanism: mechanism), let imgUrlStr = account.imageUrl, let url = URL(string: imgUrlStr) {
-                self.logoImageView?.downloadImageFromUrl(url: url)
-                self.issuerLabelTopConstraint?.constant = 145
+                self.logoImageView.downloadImageFromUrl(url: url)
+                self.issuerLabelTopConstraint.constant = 145
             }
         }
         else {
-            self.issuerLabel?.text = ""
-            self.accountNameLabel?.text = ""
+            self.issuerLabel.text = ""
+            self.accountNameLabel.text = ""
         }
     }
     
@@ -90,13 +147,43 @@ class NotificationRequestViewController: BaseViewController {
             self.dismiss(animated: true)
             return
         }
-        
-        notification.accept(onSuccess: {
-            DispatchQueue.main.async {
-                self.dismiss(animated: true)
+        if notification.pushType == .default {
+            notification.accept(onSuccess: {
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true)
+                }
+            }) { (error) in
+                self.displayAlert(title: "Error", message: error.localizedDescription)
             }
-        }) { (error) in
-            self.displayAlert(title: "Error", message: error.localizedDescription)
+        } else if notification.pushType == .biometric {
+            notification.accept(title: "Please authenticate", allowDeviceCredentials: true) {
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true)
+                }
+            } onError: { error in
+                self.displayAlert(title: "Error", message: error.localizedDescription)
+            }
+        } else {
+            self.displayAlert(title: "Error", message: "Push Type not supported")
+        }
+    }
+    
+    @objc func numberButtonTapped(sender : UIButton) {
+        guard let notification = self.notification,
+              let selectedNumber = sender.title(for: .normal) else {
+            self.dismiss(animated: true)
+            return
+        }
+        if notification.pushType == .challenge {
+            notification.accept(challengeResponse: selectedNumber, onSuccess: {
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true)
+                }
+            }) { (error) in
+                self.displayAlert(title: "Error", message: error.localizedDescription)
+            }
+        } else {
+            self.displayAlert(title: "Error", message: "Push Type not supported")
         }
     }
     
@@ -115,4 +202,18 @@ class NotificationRequestViewController: BaseViewController {
             self.displayAlert(title: "Error", message: error.localizedDescription)
         }
     }
+}
+
+extension NotificationRequestViewController {
+    func getAddressFrom(latitude: Double, longitude: Double, handler: @escaping (String?) -> Void) {
+        let location: CLLocation = CLLocation(latitude: latitude, longitude: longitude)
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error ) in
+            if error == nil, let placemark = placemarks?.first {
+                handler("\(placemark.locality ?? ""), \(placemark.country ?? "")")
+            } else {
+                handler(nil)
+            }
+        }
+    }
+    
 }
