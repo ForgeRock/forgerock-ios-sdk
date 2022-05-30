3cb7d0b9e6e23dfa6cb8b1ed9e91d2c483ea342a
jey <jey.periyasamy@forgerock.com>
2022-05-30 11:09:59 -0500

    we have decided to go with serial queue and sync approach

---
+++ FRAuth/FRAuthTests/FRAuthSwiftTests/FRAuth/Device/AtomicDictionaryTests.swift
@@ -0,0 +1,28 @@
+//
+//  AtomicStorageTest.swift
+//  FRAuthTests
+//
+//  Copyright (c) 2022 ForgeRock. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
+//
+
+
+import XCTest
+@testable import FRAuth
+
+class AtomicDictionaryTests: XCTestCase {
+
+    func testAtomicStorage() {
+       let storage = AtomicDictionary()
+        DispatchQueue.concurrentPerform(iterations: 2) { (index) in
+            storage.set(key: "\(index)", value: ["key":  "value"])
+        }
+    
+        XCTAssertEqual(storage.get()["0"] as! [String : String], ["key":  "value"])
+        XCTAssertEqual(storage.get()["1"] as! [String : String], ["key":  "value"])
+    }
+
+}
+
