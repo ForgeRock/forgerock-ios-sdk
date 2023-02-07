2cf79e3f8e9ec194002a5990f003688dec3b73b8
jey <jey.periyasamy@forgerock.com>
2023-02-06 18:42:56 -0600

    updating a UI to support username+authType+date

--- FRAuth/FRAuthTests/FRAuthSwiftTests/FRAuth/DeviceBinding/KeychainDeviceRepositoryTests.swift
+++ FRAuth/FRAuthTests/FRAuthSwiftTests/FRAuth/DeviceBinding/KeychainDeviceRepositoryTests.swift
@@ -20,22 +20,36 @@ class KeychainDeviceRepositoryTests: XCTestCase {
         let userName = "User Name"
         let key = "Test Key 1"
         let authenticationType = DeviceBindingAuthenticationType.none
-        let sharedPreferencesDeviceRepository = KeychainDeviceRepository()
+        let sharedPreferencesDeviceRepository = KeychainDeviceRepository(uuid: nil, keychainService: nil)
+        let createdAt = Date().timeIntervalSince1970
         
         do {
-            let uuid = try sharedPreferencesDeviceRepository.persist(userId: userId, userName: userName, key: key, authenticationType: authenticationType)
+            let uuid = try sharedPreferencesDeviceRepository.persist(userId: userId, userName: userName, key: key, authenticationType: authenticationType, createdAt: createdAt)
             XCTAssertFalse(uuid.isEmpty)
             
             let allKeys = sharedPreferencesDeviceRepository.getAllKeys()
             XCTAssertNotNil(allKeys)
             
-            let userKeyJson = allKeys![key] as? String
+            let userKeyJson = allKeys![key]
             XCTAssertNotNil(userKeyJson)
             
-            XCTAssertTrue(userKeyJson!.contains(userId))
-            XCTAssertTrue(userKeyJson!.contains(userName))
-            XCTAssertTrue(userKeyJson!.contains(uuid))
-            XCTAssertTrue(userKeyJson!.contains(authenticationType.rawValue))
+            let data = (userKeyJson as? String)?.data(using: .utf8)
+            let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+            
+            
+            let actualUserId = json[KeychainDeviceRepository.userIdKey] as! String
+            let actualUserName = json[KeychainDeviceRepository.userNameKey] as! String
+            let actualKid = json[KeychainDeviceRepository.kidKey] as! String
+            let actualAuthTypeString = json[KeychainDeviceRepository.authTypeKey] as! String
+            let actualAuthType = DeviceBindingAuthenticationType(rawValue: actualAuthTypeString)
+            let actualCreatedAt = json[KeychainDeviceRepository.createdAtKey] as! Double
+        
+            XCTAssertTrue(actualUserId == userId)
+            XCTAssertTrue(actualUserName == userName)
+            XCTAssertTrue(actualKid == uuid)
+            XCTAssertTrue(actualAuthType == authenticationType)
+            XCTAssertTrue(actualCreatedAt == createdAt)
+
         } catch {
             XCTFail("Failed to persist user info")
         }
@@ -47,10 +61,11 @@ class KeychainDeviceRepositoryTests: XCTestCase {
         let userName = "User Name"
         let key = "Test Key 2"
         let authenticationType = DeviceBindingAuthenticationType.none
-        let sharedPreferencesDeviceRepository = KeychainDeviceRepository()
+        let sharedPreferencesDeviceRepository = KeychainDeviceRepository(uuid: nil, keychainService: nil)
+        let createdAt = Date().timeIntervalSince1970
         
         do {
-            let uuid = try sharedPreferencesDeviceRepository.persist(userId: userId, userName: userName, key: key, authenticationType: authenticationType)
+            let uuid = try sharedPreferencesDeviceRepository.persist(userId: userId, userName: userName, key: key, authenticationType: authenticationType, createdAt: createdAt)
             XCTAssertFalse(uuid.isEmpty)
             
             var allKeys = sharedPreferencesDeviceRepository.getAllKeys()
