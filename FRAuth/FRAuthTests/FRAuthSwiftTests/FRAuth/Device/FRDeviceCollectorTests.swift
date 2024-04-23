a40730df17280dfde2486ac7d657a5e1537750d7
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2024-04-23 13:04:38 -0400

    Update copyright

--- FRAuth/FRAuthTests/FRAuthSwiftTests/FRAuth/Device/FRDeviceCollectorTests.swift
+++ FRAuth/FRAuthTests/FRAuthSwiftTests/FRAuth/Device/FRDeviceCollectorTests.swift
@@ -2,7 +2,7 @@
 //  FRDeviceCollectorTests.swift
 //  FRAuthTests
 //
-//  Copyright (c) 2019 ForgeRock. All rights reserved.
+//  Copyright (c) 2019-2024 ForgeRock. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
@@ -81,7 +81,7 @@ class FRDeviceCollectorTests: FRAuthBaseTest {
         XCTAssertNotNil((hardware["cpu"] as? Int))
 //        XCTAssertNotNil((hardware["activeCPU"] as? Int))
         XCTAssertNotNil((hardware["memory"] as? Int))
-//        XCTAssertNotNil((hardware["storage"] as? Int))
+//        XCTAssertNotNil((hardware["storage"] as? Int)) //has been removed as Apple doesn't allow the use of this API for fingerprinting
         XCTAssertNotNil((hardware["manufacturer"] as? String))
         
         // Then, validate display section from hardware
