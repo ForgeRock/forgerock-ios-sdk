4cd2328ab404e15514c0f521e52f490a7798e55f
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2025-01-20 13:13:18 -0500

    Update copyright

--- FRCore/FRCore/Security/DyldDetector.swift
+++ FRCore/FRCore/Security/DyldDetector.swift
@@ -2,7 +2,7 @@
 //  DyldDetector.swift
 //  FRCore
 //
-//  Copyright (c) 2019-2025 ForgeRock. All rights reserved.
+//  Copyright (c) 2019-2025 Ping Identity. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
