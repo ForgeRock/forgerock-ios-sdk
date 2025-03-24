22d2f542da843acc75d7a1c514dee9c17ba9cca6
George Bafaloukas <george.bafaloukas@forgerock.com>
2025-03-24 15:09:15 +0000

    Updating copyrights of WebAuthnCallback.swift

--- FRAuth/FRAuth/Callbacks/WebAuthnCallback.swift
+++ FRAuth/FRAuth/Callbacks/WebAuthnCallback.swift
@@ -2,7 +2,7 @@
 //  WebAuthnCallback.swift
 //  FRAuth
 //
-//  Copyright (c) 2021 ForgeRock. All rights reserved.
+//  Copyright (c) 2021-2025 ForgeRock. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
