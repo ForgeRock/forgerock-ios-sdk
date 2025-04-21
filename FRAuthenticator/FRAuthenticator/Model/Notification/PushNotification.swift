5f9bf34bea70bd7a80e64811faa89017137acacb
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2025-04-20 22:13:26 -0400

    Update copyright for all files

--- FRAuthenticator/FRAuthenticator/Model/Notification/PushNotification.swift
+++ FRAuthenticator/FRAuthenticator/Model/Notification/PushNotification.swift
@@ -2,7 +2,7 @@
 //  Notification.swift
 //  FRAuthenticator
 //
-//  Copyright (c) 2020-2023 ForgeRock. All rights reserved.
+//  Copyright (c) 2020 - 2025 Ping Identity Corporation. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
--- e2e/FRAuthenticatorExample/FRAuthenticatorExample/Cell/BaseTableViewCell.swift
+++ e2e/FRAuthenticatorExample/FRAuthenticatorExample/Cell/BaseTableViewCell.swift
@@ -2,7 +2,7 @@
 //  BaseTableViewCell.swift
 //  FRAuthenticatorExample
 //
-//  Copyright (c) 2020 ForgeRock. All rights reserved.
+//  Copyright (c) 2020 - 2025 Ping Identity Corporation. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
