15d970eee38e883235d5e132890e9cf2b7b88429
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2025-04-20 22:13:26 -0400

    Update copyright for all files

--- FRAuth/FRAuth/Authentication/Node.swift
+++ FRAuth/FRAuth/Authentication/Node.swift
@@ -2,7 +2,7 @@
 //  Node.swift
 //  FRAuth
 //
-//  Copyright (c) 2019-2024 ForgeRock. All rights reserved.
+//  Copyright (c) 2019 - 2025 Ping Identity Corporation. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
--- e2e/FRExample/FRExample/Playground.swift
+++ e2e/FRExample/FRExample/Playground.swift
@@ -2,7 +2,7 @@
 //  Playground.swift
 //  FRExample
 //
-//  Copyright (c) 2023 ForgeRock. All rights reserved.
+//  Copyright (c) 2023 - 2025 Ping Identity Corporation. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
