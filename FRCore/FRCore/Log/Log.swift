0b5e754abe9785acb11df0ef01f64b33765cc22d
Stoyan Petrov <stoyan@petrov.ca>
2026-03-06 12:22:07 -0800

    SDKS-4822 Release activities for ForgeRock iOS SDK 4.8.5 (#365)

--- FRCore/FRCore/Log/Log.swift
+++ FRCore/FRCore/Log/Log.swift
@@ -129,7 +129,7 @@ public class Log: NSObject {
     //  MARK: - Property
     
     /// Current SDK version. We hard code it here as currently there is no other way to get it dinamically when used with SPM
-    public static let sdkVersion = "4.8.4"
+    public static let sdkVersion = "4.8.5"
     /// Current LogLevel
     static var logLevel: LogLevel = .none
     /// Current Loggers to handle log entries
